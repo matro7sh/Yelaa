02f67430ee6f920b917594712ec4f3485a714674
Julien Arrault <julien.arrault@orangecyberdefense.com>
2021-12-16 08:20:02 -0500

    merge domains into one result

--- tool/subfinder.go
+++ tool/subfinder.go
@@ -4,20 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-
-	"github.com/fatih/color"
 )
 
 func Subfinder(domain string, filename string) {
-	arg := "-d"
-	out, err := exec.Command("subfinder", arg, domain).Output()
+	out, err := exec.Command("subfinder", "-d", domain).Output()
 
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
-
-	output := string(out[:])
-	color.Green(output)
 
 	err = os.WriteFile(filename, out, 0644)
 
