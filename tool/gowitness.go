ff40c43242a37157bdbd8dc30bfb9fd419f8c088
enaylal <jenaye@protonmail.com>
2021-12-18 23:49:18 +0100

    fix path

--- tool/gowitness.go
+++ tool/gowitness.go
@@ -2,18 +2,20 @@ package tool
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/fatih/color"
 )
 
 func Gowitness(urls string) {
+	UserHomeDir, err := os.UserHomeDir()
 	args := "file"
 	args2 := "-f"
 	args3 := urls
 
 	args4 := "--screenshot-path"
-	args5 := "~/.yelaa/screenshots"
+	args5 := UserHomeDir + "/.yelaa/screenshots"
 	out, err := exec.Command("gowitness", args, args2, args3, args4, args5).Output()
 
 	if err != nil {
