b31a747af4814897c488f30306468a2ac1f91904
darkweak <darkweak@protonmail.com>
2022-01-09 23:11:40 +0100

    Fix httpx ports scan

--- tool/httpx.go
+++ tool/httpx.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/fatih/color"
 
+	customport "github.com/projectdiscovery/httpx/common/customports"
 	"github.com/projectdiscovery/httpx/runner"
 )
 
@@ -30,9 +31,12 @@ func (h *Httpx) Configure(config interface{}) {
 	var httpxconfiguration HttpxConfiguration
 	_ = json.Unmarshal(b, &httpxconfiguration)
 	h.configuration = httpxconfiguration
+	customPorts := customport.CustomPorts{}
+	customPorts.Set("25,80,81,135,389,443,1080,3000,3306,8080,8443,8888,9090,8089")
 
 	opts := runner.Options{
 		InputFile:         httpxconfiguration.Input,
+		CustomPorts:       customPorts,
 		ExtractTitle:      true,
 		ContentLength:     true,
 		OutputContentType: true,
@@ -43,6 +47,7 @@ func (h *Httpx) Configure(config interface{}) {
 		FollowRedirects:   true,
 		Retries:           2,
 		Threads:           50,
+		Timeout:           8,
 	}
 
 	if httpxconfiguration.Output != "" {
@@ -56,5 +61,6 @@ func (h *Httpx) Run(_ string) {
 	h.runnerInstance.RunEnumeration()
 
 	body, _ := ioutil.ReadFile(h.configuration.Output)
-	fmt.Println(ioutil.WriteFile(h.configuration.Output, regexp.MustCompile(`( \[.+)`).ReplaceAll(body, []byte{}), 0755))
+	fmt.Println(string(body))
+	ioutil.WriteFile(h.configuration.Output, regexp.MustCompile(`( \[.+)`).ReplaceAll(body, []byte{}), 0755)
 }
